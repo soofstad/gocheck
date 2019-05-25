67f740c4d315900fc279cef269f747fbae075034
Stig Ofstad <stigofstad@gmail.com>
2019-05-25 16:13:42 +0200

    save and edit

--- src/gocheck/gocheck.go
+++ src/gocheck/gocheck.go
@@ -13,29 +13,48 @@ type Page struct {
 }
 
 func (p *Page) save() error {
-	filename := p.Title + ".txt"
+	filename := "data/" + p.Title + ".txt"
 	return ioutil.WriteFile(filename, p.Body, 0600)
 }
 
 func renderTemplate(writer http.ResponseWriter, tmpl string, page *Page) {
-	templ, _ := template.ParseFiles("tmpl" + ".html")
+	templ, err := template.ParseFiles("templates/" + tmpl + ".html")
+	if err != nil {
+		http.Error(writer, err.Error(), http.StatusInternalServerError)
+		return
+	}
 	templ.Execute(writer, page)
 }
 
 func loadPage(title string) (*Page, error) {
-	filename := title + ".txt"
+	filename := "data/" + title + ".txt"
 	body, err := ioutil.ReadFile(filename)
 	if err != nil {
 		return nil, err
 	}
-
 	return &Page{Title: title, Body: body}, nil
 }
 
 func viewHandler(writer http.ResponseWriter, request *http.Request) {
 	title := request.URL.Path[len("/view/"):]
-	page, _ := loadPage(title)
+	page, err := loadPage(title)
+	if err != nil {
+		http.Redirect(writer, request, "/edit/"+title, http.StatusFound)
+		return
+	}
 	renderTemplate(writer, "view", page)
+}
+
+func saveHandler(writer http.ResponseWriter, request *http.Request) {
+	title := request.URL.Path[len("/save/"):]
+	body := request.FormValue("body")
+	page := &Page{Title: title, Body: []byte(body)}
+	err := page.save()
+	if err != nil {
+		http.Error(writer, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	http.Redirect(writer, request, "/view/"+title, http.StatusFound)
 }
 
 func editHandler(writer http.ResponseWriter, request *http.Request) {
@@ -50,6 +69,6 @@ func editHandler(writer http.ResponseWriter, request *http.Request) {
 func main() {
 	http.HandleFunc("/view/", viewHandler)
 	http.HandleFunc("/edit/", editHandler)
-	// http.HandleFunc("/save/", saveHandler)
+	http.HandleFunc("/save/", saveHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
